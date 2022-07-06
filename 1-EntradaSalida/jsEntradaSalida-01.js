8428a30492a2e6574d7cb01efa86091946ed362e
Leandro Emanuel Torrez <108636036+LeanTorrez@users.noreply.github.com>
2022-07-06 15:15:15 -0300

    nuevo

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Division Z");
+	alert("Esto funciona de maravilla");
 }
 
